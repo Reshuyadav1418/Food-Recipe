8bdaa853889c701730132ac0f0382ce6bf56d1c4
Reshuyadav1418 <reshuyadav1409@gmail.com>
2026-02-14 20:43:40 +0530

    shows fetch data

--- frontend/src/components/RecipeItems.jsx
+++ frontend/src/components/RecipeItems.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-
+import food from '../assets/food.jpg';
+import {BsStopwatchFill} from 'react-icons/bs';
+import { FaHeart } from "react-icons/fa6";
 const RecipeItems = () => {
     const allRecipes=useLoaderData();
     console.log(allRecipes);
   return (
-    <div>RcipeItems</div>
+    <div>
+      <div className="card-container">
+        {
+          allRecipes?.map((item,index)=>{
+            return(
+              <div key={index} className='card'>
+                <img src={food} width="120px" height="100px"></img>
+                <div className="card-body">
+                  <div className="title">{item.title}</div>
+                  <div className="icons">
+                    <div className="timer"><BsStopwatchFill/></div>
+                    <FaHeart />
+                  </div>
+                </div>
+                </div>
+            )
+          })
+        }
+      </div>
+
+
+    </div>
    
   );
 }
